0c6fcfce1307944704c2cc50a195e6669aec8d10
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-01-29 15:46:33 -0500

    complete for_each method, first pass

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -14,16 +14,21 @@ class BinarySearchTree:
     def insert(self, value):
         # each object should be its own BST
         bst_value = BinarySearchTree(value)
+        # if lower than current node
         if self.value >= value:
+            # if it doesnt have a child, this value becomes child
             if not self.left:
                 self.left = bst_value
             else:
-            # check against right value
+            # recursion with the left child
                 return self.left.insert(value)
+        # if greater than current node
         elif self.value < value:
+            # if it doesnt have child, this value becomes child
             if not self.right:
                 self.right = bst_value
             else:
+                # recursion with the right child
                 return self.right.insert(value)
 
     # Return True if the tree contains the value
@@ -33,37 +38,21 @@ class BinarySearchTree:
         if self.value == target:
             # return true
             return True
-        
+
         # if current value is higher than target
         elif self.value > target:
             if self.left:
                 return self.left.contains(target)
-            else:
-                return False
         elif self.value < target:
             if self.right:
                 return self.right.contains(target)
-            else:
-                return False
-
-       
-          # if there is a value to the right:
-            # recursion to the right
-          # if no value:
-            # return false
-        # if current value is lesser than target
-            # if there is a value to left
-                # recursion to the left
-
-            # if no value
-            #   # return false 
+        return False
 
     # Return the maximum value found in the tree
     def get_max(self):
         highest = self.value
         current = self
         while current.right:
-            print(highest)
             current = current.right
         highest = current.value
         return highest
@@ -71,8 +60,18 @@ class BinarySearchTree:
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        current = self.value
+        right = self.right
+        left = self.left
 
+        self.value = cb(current)
+        print(self.value)
+        if right:
+            right.for_each(cb)
+        
+        if left:
+            left.for_each(cb)
+        
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
